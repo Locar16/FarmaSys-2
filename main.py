ab6d52d3275f42ca1dba790666e9df5a0e680eb5
Locar16 <locar1608@gmail.com>
2026-08-19 14:51:18 -0300

    add Suporte para muitos parametros

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import sys
+
 def main():
-    print("Hello World")
+    if len(sys.argv) > 1:
+        for item in sys.argv[1:]:
+            print("Olá, ", item + "!")
+    else:
+        print("Olá mundo!")
 if __name__ == "__main__":
     main()
